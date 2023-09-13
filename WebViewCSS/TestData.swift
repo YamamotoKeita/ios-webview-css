c4293eaa44a15fe89a74a60e5c00e1140707a214
Keita Yamamoto <yamamoto@altonotes.co.jp>
2023-09-13 14:52:36 +0900

    First commit

---
+++ WebViewCSS/TestData.swift
@@ -0,0 +1,36 @@
+//
+//  TestData.swift
+//  WebViewCSS
+//
+//  Created by Keita Yamamoto on 2023/09/13.
+//
+
+import Foundation
+
+struct TestData {
+    static let prOfferBody = """
+<div class="p-pr-offer-body__highlights">
+    <h2>おすすめポイント</h2>
+    <ol>
+    <li>ほげほげグループ企業</li>
+    <li>年間休日124日。巨大グループの福利厚生</li>
+    <li>大都市ランドマークや都市再開発に携わる</li>
+    </ol>
+</div>
+山田1640454 太郎1640454様<br>
+<br>
+こんにちは、エージェントのスカウト担当です。
+<br>この度、<span class="p-pr-offer-body__font-red">新たに募集を開始した企業の8月追加推薦枠</span>へご案内いたします。<br><br>推薦でご案内させていただくため<br>
+<span class="u-font-bold">”一次面接免除やいきなり最終選考で進める企業もあり、最短1週間での内定獲得”</span>も狙えます。<br><br>
+<span class="u-font-bold">◆大手級の待遇例(一部)</span><br><br><br>
+<span class="p-pr-offer-body__under-line">※締め切りが迫っているため、まずはご予約されることをお勧めいたします！</span><br>各企業、募集には期限が設けられているため、後日のエントリーだとご希望に沿った求人がなくなってしまう可能性がございます。
+<table class="p-pr-offer-body__outline">
+    <caption>募集概要</caption>
+    <tbody>
+    <tr><th>会社名</th><td>株式会社◯◯</td></tr>
+    <tr><th>募集年次</th><td>2024年卒</td></tr>
+    <tr><th>募集対象</th><td>2024年度卒業予定の学生</td></tr>
+    </tbody>
+</table>
+"""
+}
---
+++ WebViewCSS/ViewController.swift
@@ -0,0 +1,94 @@
+import UIKit
+import WebKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var containerView: UIView!
+
+    var webView: WKWebView?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        load()
+    }
+
+    func load() {
+        let webView = createWebView()
+        if let htmlPath = Bundle.main.path(forResource: "pr-offer-quest", ofType: "html") {
+            let htmlUrl = URL(fileURLWithPath: htmlPath, isDirectory: false)
+            webView.loadFileURL(htmlUrl, allowingReadAccessTo: htmlUrl)
+        }
+    }
+
+    func createWebView() -> WKWebView {
+        self.webView?.removeFromSuperview()
+
+        let config = WKWebViewConfiguration()
+        let webView = WKWebView(frame: CGRect.zero, configuration: config)
+        webView.isInspectable = true
+        webView.navigationDelegate = self
+        self.webView = webView
+
+        containerView.addSubview(webView)
+
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        webView.leadingAnchor.constraint(equalTo:containerView.leadingAnchor).isActive = true
+        webView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor).isActive = true
+        webView.topAnchor.constraint(equalTo: containerView.topAnchor).isActive = true
+        webView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor).isActive = true
+        return webView
+    }
+
+    func appendStyleSheet(_ css: String) {
+        let escapedCss = escapeForStringLiteral(css)
+        var script = "var style = document.createElement('style');"
+        script += "style.type = 'text/css';"
+        script += "var content = document.createTextNode('\(escapedCss)');"
+        script += "style.appendChild(content);"
+        script += "document.body.appendChild(style);"
+        webView?.evaluateJavaScript(script)
+    }
+
+    func appendBody() {
+        let escaped = escapeForStringLiteral(TestData.prOfferBody)
+        let script = "document.body.innerHTML = '\(escaped)';"
+        webView?.evaluateJavaScript(script)
+    }
+
+    func escapeForStringLiteral(_ src: String) -> String {
+        let stringLiteralConversion = [
+            "\r": "",
+            "\n": "\\n",
+            "\"": "\\\"",
+            "\'": "\\'",
+            "\t": "\\t",
+        ]
+        var result = src
+        stringLiteralConversion.forEach {key, value in
+            result = result.replacingOccurrences(of: key, with: value)
+        }
+        return result
+    }
+
+    func readResourceString(name: String, type: String) -> String? {
+        if let path = Bundle.main.path(forResource: name, ofType: type) {
+            do {
+                return try String(contentsOfFile: path)
+            } catch  {
+                print(error)
+            }
+        }
+        return nil
+    }
+}
+
+extension ViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        if webView.url?.absoluteString == "about:blank" { return }
+
+        if let css = readResourceString(name: "pr-offer-quest", type: "css") {
+            appendStyleSheet(css)
+        }
+        appendBody()
+    }
+}
